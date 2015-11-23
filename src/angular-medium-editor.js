9ee98f0f1fcb5a54562f6bfbfb28a10fbbdf809b
Thijs Wijnmaalen <thijs@wijnmaalen.name>
2015-11-23 21:42:46 +0100

    Rework directive to integrate better with ngModel #34

--- src/angular-medium-editor.js
+++ src/angular-medium-editor.js
@@ -1,84 +1,51 @@
+/*global MediumEditor */
 'use strict';
 
 angular.module('angular-medium-editor', [])
 
   .directive('mediumEditor', function() {
 
+    function toInnerText(value) {
+      var tempEl = document.createElement('div'),
+          text;
+      tempEl.innerHTML = value;
+      text = tempEl.textContent || '';
+      return text.trim();
+    }
+
     return {
       require: 'ngModel',
       restrict: 'AE',
       scope: { bindOptions: '=' },
-      link: function(scope, iElement, iAttrs, ctrl) {
+      link: function(scope, iElement, iAttrs, ngModel) {
 
         angular.element(iElement).addClass('angular-medium-editor');
 
-        // Parse options
-        var opts = {},
-            placeholder = '';
-        var prepOpts = function() {
-          if (iAttrs.options) {
-            opts = scope.$eval(iAttrs.options);
-          }
-          var bindOpts = {};
-          if (scope.bindOptions !== undefined) {
-            bindOpts = scope.bindOptions;
-          }
-          opts = angular.extend(opts, bindOpts);
+        // Global MediumEditor
+        ngModel.editor = new MediumEditor(iElement, scope.bindOptions);
+
+        ngModel.$render = function() {
+          iElement.html(ngModel.$viewValue || "");
+          ngModel.editor.getExtensionByName('placeholder').updatePlaceholder(iElement[0]);
         };
-        prepOpts();
-        placeholder = opts.placeholder;
-        scope.$watch('bindOptions', function() {
-          // in case options are provided after mediumEditor directive has been compiled and linked (and after $render function executed)
-          // we need to re-initialize
-          if (ctrl.editor) {
-            ctrl.editor.destroy();
+
+        ngModel.$isEmpty = function(value) {
+          if (/[<>]/.test(value)) {
+            return toInnerText(value).length === 0;
+          } else if (value) {
+            return value.length === 0;
+          } else {
+            return true;
           }
-          prepOpts();
-          // Hide placeholder when the model is not empty
-          if (!ctrl.$isEmpty(ctrl.$viewValue)) {
-            opts.placeholder = '';
-          }
-          ctrl.editor = new MediumEditor(iElement, opts);
+        };
+
+        ngModel.editor.subscribe('editableInput', function (event, editable) {
+          ngModel.$setViewValue(editable.innerHTML.trim());
         });
 
-        var onChange = function() {
-
-          scope.$apply(function() {
-
-            // If user cleared the whole text, we have to reset the editor because MediumEditor
-            // lacks an API method to alter placeholder after initialization
-            if (iElement.html() === '<p><br></p>' || iElement.html() === '') {
-              opts.placeholder = placeholder;
-              var editor = new MediumEditor(iElement, opts);
-            }
-
-            ctrl.$setViewValue(iElement.html());
-          });
-        };
-
-        // view -> model
-        iElement.on('blur', onChange);
-        iElement.on('input', onChange);
-        iElement.on('paste', onChange);
-
-        // model -> view
-        ctrl.$render = function() {
-
-          if (!this.editor) {
-            // Hide placeholder when the model is not empty
-            if (!ctrl.$isEmpty(ctrl.$viewValue)) {
-              opts.placeholder = '';
-            }
-
-            this.editor = new MediumEditor(iElement, opts);
-          }
-
-          iElement.html(ctrl.$isEmpty(ctrl.$viewValue) ? '' : ctrl.$viewValue);
-          
-          // hide placeholder when view is not empty
-          if(!ctrl.$isEmpty(ctrl.$viewValue)) angular.element(iElement).removeClass('medium-editor-placeholder'); 
-        };
-
+        scope.$watch('bindOptions', function(bindOptions) {
+          ngModel.editor.init(iElement, bindOptions);
+        });
       }
     };
 
